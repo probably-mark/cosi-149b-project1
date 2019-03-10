563aaf103c013bf8ed83d52020f7ab7dc3f156ae
eureyuri <eureynoguchi@gmail.com>
2019-03-10 16:17:41 -0400

    Attemped to train on gcloud

--- opencv-text-recognition/ourtry.py
+++ opencv-text-recognition/ourtry.py
@@ -97,7 +97,7 @@ ap.add_argument("-east", "--east", type=str, required=True,
                 help="path to input EAST text detector")
 ap.add_argument("-v", "--video", type=str,
                 help="path to optinal input video file")
-ap.add_argument("-c", "--min-confidence", type=float, default=0.5,
+ap.add_argument("-c", "--min-confidence", type=float, default=0.3,
                 help="minimum probability required to inspect a region")
 ap.add_argument("-w", "--width", type=int, default=320,
                 help="resized image width (should be multiple of 32)")
